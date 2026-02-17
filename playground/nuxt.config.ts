ea36111ed80512022c242703ce076aba0516627a
hareland <packagist@proton.me>
2026-02-17 19:51:24 +0100

    feat(config): warn if ios config is missing during build and update .gitignore

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,15 @@
+import { existsSync } from 'node:fs'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: '2025-07-15',
+  hooks: {
+    'build:before'() {
+      if (!existsSync('ios/App/App/capacitor.config.json')) {
+        console.warn('ios/App/App/capacitor.config.json not found. Add using npx cap add ios')
+      }
+    },
+  },
   capacitor: {
     config: {
       appId: 'com.example.playground',
