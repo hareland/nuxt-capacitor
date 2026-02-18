3aa95fe1eeb1cc4b3a4ff2d2c8b68561fe8f4022
hareland <packagist@proton.me>
2026-02-18 17:55:01 +0100

    chore(lint): update ignores to include `.starters` directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
-const ignores = ['./playground/ios/**', './playground/android/**', './playground/electron/**']
+const ignores = ['./playground/ios/**', './playground/android/**', './playground/electron/**', './.starters/**']
 
 export default createConfigForNuxt({
   dirs: {
