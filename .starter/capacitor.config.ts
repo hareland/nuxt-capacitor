f9cc7c69a3ffb081acaa6353711006e55143a406
hareland <packagist@proton.me>
2026-02-18 17:11:41 +0100

    chore(config): remove unused Capacitor configuration files

--- .starter/capacitor.config.ts
+++
@@ -1,5 +0,0 @@
-import { defineCapacitorConfig } from './.nuxt/capacitor.mjs'
-
-export default defineCapacitorConfig({
-  // Add your overrides here, or configure via nuxt.config.ts > capacitor: {}
-})
