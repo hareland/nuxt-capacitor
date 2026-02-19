06c877436b8b8ddc251400da98cb67ff2727167e
hareland <packagist@proton.me>
2026-02-19 17:00:43 +0100

    feat(konsta-ui): add dark/light theme toggle and update navbar style

--- .starters/konsta-ui/app/pages/index.vue
+++ .starters/konsta-ui/app/pages/index.vue
@@ -5,47 +5,35 @@ import {
   kNavbar,
   kBlock,
   kButton,
-  kList,
-  kListItem,
   kBlockTitle,
 } from 'konsta/vue'
 
-const onButtonClick = (button: string) => () => {
-  window.alert(`Button ${button} clicked!`)
+const colorMode = useColorMode()
+
+const toggleColorMode = () => {
+  colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
 }
+
 </script>
 
 <template>
   <k-page>
-    <k-navbar title="My App" />
+    <k-navbar title="My App" large/>
 
     <k-block strong>
       <p>Here is your Nuxt & Konsta UI app. Let's see what we have here.</p>
     </k-block>
     <k-block-title>Navigation</k-block-title>
-    <k-list
-      strong
-      inset
-    >
-      <k-list-item
-        href="/about/"
-        title="About"
-      />
-      <k-list-item
-        href="/form/"
-        title="Form"
-      />
-    </k-list>
 
     <k-block
-      strong
-      class="flex space-x-4"
+        strong
+        class="flex space-x-4"
     >
-      <k-button @click="onButtonClick('button-1')">
-        Button 1
+      <k-button @click="toggleColorMode">
+        Theme
       </k-button>
-      <k-button @click="onButtonClick('button-2')">
-        Button 2
+      <k-button @click="colorMode.preference = 'system'">
+        System Theme
       </k-button>
     </k-block>
   </k-page>
